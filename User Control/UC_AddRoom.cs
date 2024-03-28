508206cdcda81b09dd17724d9e757ae66f06dee7
Ishuwaraa <115783906+Ishuwaraa@users.noreply.github.com>
2024-03-28 21:47:41 +0530

    Project files added

---
+++ User Control/UC_AddRoom.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace HMS.User_Control
+{
+    public partial class UC_AddRoom : UserControl
+    {
+        public UC_AddRoom()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Db\hotelMgt.mdf;Integrated Security=True;Connect Timeout=30");
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string roomType = comboBox1.Text;
+            string bed = comboBox2.Text;
+            string room = textBox1.Text;
+            int roomNo;
+            int.TryParse(textBox1.Text, out roomNo);
+            int price;
+            int.TryParse(textBox2.Text, out price);
+
+            /*try
+            {
+                int roomNo = int.Parse(textBox1.Text);
+                int price = int.Parse(textBox2.Text);
+
+                if (roomNo != 0 && price != 0 && comboBox1.Text != "" && comboBox2.Text != "")
+                {
+                    //data base codes
+                    string qry = "insert into roomDetails values (" + roomNo + ", '" + roomType + "', '" + bed + "', " + price + ")";
+                    SqlCommand cmd = new SqlCommand(qry, conn);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    UC_AddRoom_Load(this, null);
+
+                    clearAll();
+                    conn.Close();
+                }
+                else
+                {
+                    MessageBox.Show("All Fields must be filled");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //MessageBox.Show("All Fields must be filled");
+            }*/
+
+            if (roomNo != 0 && price != 0 && roomType != "" && bed != "")
+            {
+                //data base codes
+                string qry = "insert into roomDetails values (" + roomNo + ", '" + roomType + "', '" + bed + "', " + price + ")";
+                SqlCommand cmd = new SqlCommand(qry, conn);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    UC_AddRoom_Load(this, null);
+                    clearAll();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally { conn.Close(); }
+            }
+            else
+            {
+                MessageBox.Show("All fields are required");
+            }
+        }
+
+        public void clearAll()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+        }
+
+        private void UC_AddRoom_Leave(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+
+        private void UC_AddRoom_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select * from roomDetails";
+                SqlDataAdapter sda = new SqlDataAdapter(qry, conn);
+                DataTable dTable = new DataTable();
+                sda.Fill(dTable);
+
+                dataGridView1.DataSource = dTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*try
+            {
+                int num = int.Parse(textBox1.Text);
+                string qry = "delete from roomDetails where roomNum = '" + num + "'";
+                SqlCommand cmd = new SqlCommand(qry, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                UC_AddRoom_Load(this, null);
+
+                clearAll();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message );
+            }*/
+
+            int num;
+            int.TryParse(textBox1.Text, out num);
+
+            if (num.ToString() != "" && num != 0)
+            {
+                string qry = "delete from roomDetails where roomNum = '" + num + "'";
+                SqlCommand cmd = new SqlCommand(qry, conn);
+
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    UC_AddRoom_Load(this, null);
+                    clearAll();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally { conn.Close(); }
+            }
+            else
+            {
+                MessageBox.Show("Enter the Room No");
+            }
+        }
+    }
+}
---
+++ User Control/UC_CustomerReview.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace HMS.User_Control
+{
+    partial class UC_CustomerReview
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UC_CustomerReview));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 38);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Customer Reviews";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(19, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Name: Emily Johnson";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(73, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(393, 100);
+            this.label3.TabIndex = 16;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(73, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(336, 120);
+            this.label4.TabIndex = 18;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(19, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Name: David Smith";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(575, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(348, 100);
+            this.label6.TabIndex = 20;
+            this.label6.Text = resources.GetString("label6.Text");
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(521, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(197, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Name: Sarah Martinez";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(575, 274);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(367, 100);
+            this.label8.TabIndex = 22;
+            this.label8.Text = resources.GetString("label8.Text");
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(521, 243);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(188, 20);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Name: Michael White";
+            // 
+            // UC_CustomerReview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UC_CustomerReview";
+            this.Size = new System.Drawing.Size(1016, 482);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
